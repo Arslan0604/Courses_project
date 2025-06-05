aee73f7d0066233122fdedef130525ed08f87147
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-05 18:41:20 +0500

    ars5

--- shop/views.py
+++ shop/views.py
@@ -7,4 +7,4 @@ from .models import Course
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html')
+    return render(request, 'courses.html', {'courses': courses})
