d4eb2d7a966f83808d534a82a291fa8b1f041638
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-05 16:30:54 +0500

    ars1

--- shop/views.py
+++ shop/views.py
@@ -3,11 +3,8 @@ from django.http import HttpResponse
 from .models import Course
 
 
-
-
-
 # Create your views here.
 
 def index(request):
     courses = Course.objects.all()
-    return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
+    return HttpResponse(courses)
