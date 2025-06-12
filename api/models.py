f799468b08f683dabdd9755ca598e17a910b58d4
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-12 12:49:32 +0500

    ars4

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'categories'
+        allowed_methods = ['get']
+        
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
+
 
 # Create your models here.
+
+
--- base/settings.py
+++ base/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig'
-    'api.apps.ApiConfig'
+    'shop.apps.ShopConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
