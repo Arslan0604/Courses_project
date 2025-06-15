91cad3c1ded53d4b512f49a80f1f3a2ce8041810
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-15 15:50:48 +0500

    ars9

--- base/urls.py
+++ base/urls.py
@@ -16,23 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.models import CategoryResource, CourseResource
-from tastypie.api import Api
 
-api = Api(api_name='v1')
-course_resource = CourseResource()
-category_resource = CategoryResource()
-api.register(course_resource)
-api.register(category_resource)
-
-# api/v1/courses/        GET, POST
-# api/v1/courses/1/      GET, DELETE
-# api/v1/categrories     GET
-# api/v1/categrories/1/  GET
-
-# For POST, DELETE add header
-# Key: Authorization
-# Value: ApiKey admin:asdfasd12341234
 
 
 urlpatterns = [
