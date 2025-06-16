b700865e657d740e18ef2e95b715481f86a458e2
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-16 15:53:46 +0500

    ars1

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . import models
 
+admin.site.site_header = "Course Admin"
+admin.site.site_title = "My Courses"
+admin.site.index_title = "Welcome to the Courses admin area"
+
 # Register your models here.
 admin.site.register(models.Category)
 admin.site.register(models.Course)
