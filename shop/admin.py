08f76d70338413525907628fe8499a19795de016
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-16 15:59:13 +0500

    ars1

--- shop/admin.py
+++ shop/admin.py
@@ -5,6 +5,10 @@ admin.site.site_header = "Course Admin"
 admin.site.site_title = "My Courses"
 admin.site.index_title = "Welcome to the Courses admin area"
 
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category')
+    
+
 # Register your models here.
 admin.site.register(models.Category)
-admin.site.register(models.Course)
+admin.site.register(models.Course, CourseAdmin)
