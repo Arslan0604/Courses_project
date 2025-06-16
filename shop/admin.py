caf4a7b9b71f9b367019eeff9e84339d3c6a945d
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-16 16:11:23 +0500

    ars4

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . import models
+from .models import Course, Category
 
 admin.site.site_header = "Course Admin"
 admin.site.site_title = "My Courses"
@@ -8,10 +8,22 @@ admin.site.index_title = "Welcome to the Courses admin area"
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'category')
     
+class CoursesInline(admin.TabularInline):
+    model = Course
+    
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
+    fieldsets = {
+        (None, {
+            'fields': ['title']
+            }),
+        ('Dates', {
+            'fields': ['created_at'], 
+            'classes': ['collapse']
+        })
+    }
     
 
 # Register your models here.
-admin.site.register(models.Category, CategoryAdmin)
-admin.site.register(models.Course, CourseAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Course, CourseAdmin)
